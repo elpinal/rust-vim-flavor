ad5f86bef9c6928e4e018c817fdb565ff37c1bb4
elpinal <elpinal@users.noreply.github.com>
2017-09-09 23:31:55 +0900

    Implement skip_whitespaces

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 //! A manager of Vim plugins.
 #![warn(missing_docs)]
+#![feature(ascii_ctype)]
 
 /// A parser which parses VimFlavor file.
 pub struct Parser {
@@ -7,12 +8,22 @@ pub struct Parser {
     offset: usize,
 }
 
+use std::ascii::AsciiExt;
+
 impl Parser {
     fn new(buffer: &str) -> Parser {
         Parser {
             buffer: String::from(buffer),
             offset: 0,
         }
+    }
+
+    fn skip_whitespaces(&mut self) {
+        self.offset = self.buffer
+            .bytes()
+            .skip(self.offset)
+            .position(|ch| !ch.is_ascii_whitespace())
+            .unwrap_or(self.buffer.len())
     }
 }
 
