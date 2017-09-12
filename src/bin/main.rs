cc311145b3176c21adec0f548b264abecb02d3e8
elpinal <elpinal@users.noreply.github.com>
2017-09-12 23:24:17 +0900

    Make the executable accept a file as an argument

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,17 @@
 extern crate vim_flavor;
 
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    let r = vim_flavor::install("flavor 'vspec'");
+    let name = env::args().nth(1).expect("1 argument needed");
+    let mut f = File::open(name).expect("cannot open Flavor file");
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer).expect(
+        "failed to read Flavor file",
+    );
+    let r = vim_flavor::install(&buffer);
     if let Some(e) = r.err() {
         println!("{}", e);
     }
