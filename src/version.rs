914842bac6954fb0838ca397b12838a34a473348
elpinal <elpinal@users.noreply.github.com>
2017-09-18 22:21:57 +0900

    Add from_str for Version

--- src/version.rs
+++ src/version.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use std::num;
+
 #[derive(Debug, PartialEq)]
 struct Version {
     l: usize,
@@ -10,6 +12,25 @@ struct Version {
 impl Version {
     fn new(l: usize, m: usize, n: usize) -> Version {
         Version { l, m, n }
+    }
+
+    fn from_str(s: &str) -> Result<Version, FromStrError> {
+        let v: Vec<&str> = s.split('.').collect();
+        if v.len() != 3 {
+            return Err(FromStrError::Split3);
+        }
+        Ok(Version::new(v[0].parse()?, v[1].parse()?, v[2].parse()?))
+    }
+}
+
+enum FromStrError {
+    Split3,
+    Parse(num::ParseIntError),
+}
+
+impl From<num::ParseIntError> for FromStrError {
+    fn from(e: num::ParseIntError) -> FromStrError {
+        FromStrError::Parse(e)
     }
 }
 
