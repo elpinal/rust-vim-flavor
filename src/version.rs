a8336fdab74e1999a770d38c9fba759e2aae1c4a
elpinal <elpinal@users.noreply.github.com>
2017-09-18 22:11:00 +0900

    Add test for Version::new

--- src/version.rs
+++ src/version.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+#[derive(Debug, PartialEq)]
 struct Version {
     l: usize,
     m: usize,
@@ -9,5 +10,15 @@ struct Version {
 impl Version {
     fn new(l: usize, m: usize, n: usize) -> Version {
         Version { l, m, n }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        assert_eq!(Version::new(1, 2, 3), Version { l: 1, m: 2, n: 3 });
     }
 }
